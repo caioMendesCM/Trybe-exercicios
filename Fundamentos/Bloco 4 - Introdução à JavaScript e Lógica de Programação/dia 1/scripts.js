cf2a2ab75f990f1dec36ab853adcf0c84b74f9cc
Caio Mendes <cacammoreno@gmail.com>
2022-02-02 15:39:52 -0300

    substitui exercicios 2 pelo 3 dia 4.1

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia 1/scripts.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia 1/scripts.js
@@ -1,8 +1,9 @@
-let base = 5;
-let height = 8;
+let nota = 100;
 
-let area = base*height;
-let perimeter = (base*2)+(height*2);
-
-console.log(area);
-console.log(perimeter);
+if (nota >= 80) {
+  console.log("você foi aprovado(a)!");
+} else if (nota < 80 && nota >= 60) {
+    console.log("você está na nossa lista de espera.");
+} else {
+    console.log("você foi reprovado(a).");
+}
