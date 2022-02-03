4f26038d8be52f965ac5820c61e0867df7e62a6d
Caio Mendes <cacammoreno@gmail.com>
2022-02-03 16:30:58 -0300

    adiciona exercícios bonus

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia 2/scripts.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia 2/scripts.js
@@ -72,3 +72,49 @@ console.log("Exercício 9:\n");
 for (let number of numbers2) {
   console.log(number / 2 + "\n");
 }
+
+// BÔNUS
+
+console.log("Exercício bônus 1:\n");
+let holder;
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (numbers[i] < numbers[j]) {
+      holder = numbers[j];
+      numbers[j] = numbers[i];
+      numbers[i] = holder;
+    }
+  }
+}
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i] + "\n");
+}
+
+console.log("Exercício bônus 2:\n");
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (numbers[i] > numbers[j]) {
+      holder = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = holder;
+    }
+  }
+}
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i] + "\n");
+}
+
+console.log("Exercício bônus 3:\n");
+let numbers3 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbersNew = [];
+for (let i = 0; i < numbers3.length; i++) {
+  if (i != numbers3.length - 1) {
+    numbersNew.push(numbers3[i] * numbers3[i + 1]);
+  } else {
+    numbersNew.push(numbers3[i] * 2);
+  }
+}
+
+for (const number of numbersNew) {
+  console.log(number + "\n");
+}
