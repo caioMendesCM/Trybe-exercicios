d456d50a5e78d96d3e09fc8e6f27e046430abf93
Caio Mendes <cacammoreno@gmail.com>
2022-02-15 11:04:39 -0300

    exercicio dia 5.4 completo

--- Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia 4/script.js
+++ Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia 4/script.js
@@ -1,70 +1,73 @@
 let preferences = {
-  screenBackground: 'white',
-  textBackground: 'white',
-  fontSize: 'initial',
-  lineHeight: 'initial',
-  fontFamily: 'auto',
+  background: "white",
+  color: "black",
+  fontSize: "initial",
+  lineHeight: "initial",
+  font: "auto",
 };
 
 let body = document.body;
-let text = document.getElementById('text');
-let inputs = document.querySelectorAll('.userPreferences');
-let selected = document.querySelector('.selected');
-let optionsContainer = document.querySelector('.options-container');
-let options = document.querySelectorAll('.option');
+let text = document.getElementById("text");
+let inputs = document.querySelectorAll(".userPreferences");
+let selected = document.querySelector(".selected");
+let optionsContainer = document.querySelector(".options-container");
+let options = document.querySelectorAll(".option");
 
-for(let input of inputs){
-  input.addEventListener('input', changeUserPreferences);
+for (let input of inputs) {
+  input.addEventListener("input", changeUserPreferences);
 }
 
-function changeUserPreferences(event){
+function changeUserPreferences(event) {
+  console.log(event.target.className);
 
-  if(event.target.id === 'fontSize' || event.target.id === 'lineHeight'){
-    preferences[event.target.id] = event.target.value;
+  if (event.target.className.includes("color")) {
+    preferences[event.target.name.toString()] = event.target.value;
+  } else if (event.target.className.includes("number")) {
+    preferences[event.target.name] = event.target.value;
   }
-  preferences[event.target.id] = event.target.value;
+
   updatePreferences();
-  saveToLocalStorage();
+  savePreferencesToLocalStorage();
 }
 
-function saveToLocalStorage(){
-  localStorage.setItem('userPreferences' , JSON.stringify(preferences));
+function savePreferencesToLocalStorage() {
+  localStorage.setItem("userPreferences", JSON.stringify(preferences));
 }
 
-function getPreferences(){
-  preferences = JSON.parse(localStorage.getItem('userPreferences'));
+function getPreferencesFromLocalStorage() {
+  preferences = JSON.parse(localStorage.getItem("userPreferences"));
 }
 
-function updatePreferences(){
-  for(let input of inputs){
-    if(preferences[input.id] !== undefined){
-      updateText(input.name, input.id);
+function updatePreferences() {
+  for (const preference in preferences) {
+    if (!isNaN(parseInt(preferences[preference]))) {
+      text.style[preference] = `${preferences[preference]}px`;
     }
+    text.style[preference] = preferences[preference];
   }
 }
 
-function updateText(style, key){
-  text.style[style] = preferences[key];
+function toggleOptionsActive() {
+  optionsContainer.classList.toggle("active");
 }
 
-function toggleOptionsActive(){
-  optionsContainer.classList.toggle('active');
-}
-
-selected.addEventListener('click', () => {
-
+selected.addEventListener("click", () => {
   toggleOptionsActive();
-
 });
 
-options.forEach(option => {
-  option.addEventListener('click', () => {
+options.forEach((option) => {
+  option.addEventListener("click", (event) => {
     selected.innerHTML = option.querySelector("label").innerHTML;
     toggleOptionsActive();
-  })
+    preferences[event.target.firstElementChild.name] = event.target.firstElementChild.id;
+    updatePreferences();
+    savePreferencesToLocalStorage();
+  });
 });
 
-window.onload = function(){
-  getPreferences();
+window.onload = function () {
+  if (localStorage.getItem("userPreferences") !== null) {
+    getPreferencesFromLocalStorage();
+  }
   updatePreferences();
-}
+};
