179cbc7060f8d044fd691195d5220ab31aac477d
Caio Mendes <cacammoreno@gmail.com>
2022-02-14 22:03:05 -0300

    'exercicio completo + adicionais (imcompleto)'

--- Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia 4/script.js
+++ Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia 4/script.js
@@ -6,7 +6,12 @@ let preferences = {
   fontFamily: 'auto',
 };
 
+let body = document.body;
+let text = document.getElementById('text');
 let inputs = document.querySelectorAll('.userPreferences');
+let selected = document.querySelector('.selected');
+let optionsContainer = document.querySelector('.options-container');
+let options = document.querySelectorAll('.option');
 
 for(let input of inputs){
   input.addEventListener('input', changeUserPreferences);
@@ -15,15 +20,51 @@ for(let input of inputs){
 function changeUserPreferences(event){
 
   if(event.target.id === 'fontSize' || event.target.id === 'lineHeight'){
-    preferences[event.target.id] = `${event.target.value}px`;
+    preferences[event.target.id] = event.target.value;
   }
   preferences[event.target.id] = event.target.value;
+  updatePreferences();
+  saveToLocalStorage();
 }
 
 function saveToLocalStorage(){
   localStorage.setItem('userPreferences' , JSON.stringify(preferences));
 }
 
-function setPreferences(){
-  localStorage.getItem();
+function getPreferences(){
+  preferences = JSON.parse(localStorage.getItem('userPreferences'));
+}
+
+function updatePreferences(){
+  for(let input of inputs){
+    if(preferences[input.id] !== undefined){
+      updateText(input.name, input.id);
+    }
+  }
+}
+
+function updateText(style, key){
+  text.style[style] = preferences[key];
+}
+
+function toggleOptionsActive(){
+  optionsContainer.classList.toggle('active');
+}
+
+selected.addEventListener('click', () => {
+
+  toggleOptionsActive();
+
+});
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    selected.innerHTML = option.querySelector("label").innerHTML;
+    toggleOptionsActive();
+  })
+});
+
+window.onload = function(){
+  getPreferences();
+  updatePreferences();
 }
