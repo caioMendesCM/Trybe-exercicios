31c094cfaa41bd4e92bf521946e6abee77cb300f
Caio Mendes <cacammoreno@gmail.com>
2022-03-22 15:31:09 -0300

    exercicio bonus dia 9.2

--- Fundamentos/Bloco 09 - JavaScript e Testes Assíncronos/dia 2/exercicio2.js
+++ Fundamentos/Bloco 09 - JavaScript e Testes Assíncronos/dia 2/exercicio2.js
@@ -1,8 +1,9 @@
-const API_URL = 'https://api.coincap.io/v2/assets';
+const CRYPTO_API_URL = 'https://api.coincap.io/v2/assets';
+
 const cryptoListUl = document.querySelector('#cryptoList');
 
 const fetchCrypto = async () => {
-  const cryptoList = await fetch(API_URL)
+  const cryptoList = await fetch(CRYPTO_API_URL)
     .then((response) => response.json())
     .then((data) => data.data)
     .catch((e) => console.log(`Something went wrong: ${e}`));
@@ -16,12 +17,26 @@ const setList = async () => {
     .filter((coin) => coin.rank <= 10)
     .sort((a, b) => a.rank - b.rank);
 
-  top10Coins.forEach((coin) => {
+  top10Coins.forEach(async (coin) => {
     const coinLi = document.createElement('li');
-    coinLi.innerText = `${coin.name} (${coin.symbol}): $${parseFloat(coin.priceUsd).toFixed(2)}`;
+    const usdValue = parseFloat(coin.priceUsd);
+    const currency = await fetchCurrency('brl');
+    const convertedValue = usdValue*currency;
+    coinLi.innerText = `${coin.name} (${coin.symbol}): R$${convertedValue.toFixed(2)}`;
     cryptoListUl.appendChild(coinLi);
   });
 };
+
+// Bônus
+const fetchCurrency = async (currency) => {
+  const CURRENCY_API_URL = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${currency}.min.json`;
+
+  const { [currency]: value } = await fetch(CURRENCY_API_URL)
+    .then((response) => response.json())
+    .then((data) => data);
+    
+  return value;
+}
 
 window.onload = () => {
   fetchCrypto();
